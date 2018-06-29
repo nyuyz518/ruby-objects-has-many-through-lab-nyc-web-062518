8be4b56f495e34cb2c272b9bde7a0c17cfd1486a
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-29 21:43:29 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -23,6 +23,8 @@ class Doctor
   end
 
   def patients
-    appointments.map{|appointment| appointment.patient}
+    appointments.map do |appointment| 
+      appointment.patient
+    end 
   end
 end
