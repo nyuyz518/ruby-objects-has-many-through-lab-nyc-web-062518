75471652c0140a60a9ecbe375837498f4f85a59c
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-29 21:42:53 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,9 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self}
+    Appointment.all.select do |appointment| 
+      appointment.doctor == self
+    end 
   end
 
   def patients
